826fda0b86eef264ad0c60f06101b8f86a9445b2
limacami <168204032+limacami@users.noreply.github.com>
2025-03-25 11:38:50 -0300

    c

--- java.js
+++ java.js
@@ -1,20 +1,6 @@
 function verificarParOuImpar() {
-    let numero = document.getElementById("numero").value;
+    let numero = parseInt(document.getElementById("numero").value);
     let resultado = document.getElementById("resultado");
-
-    if (numero === "") {
-        resultado.textContent = "Por favor, digite um número!";
-        resultado.style.color = "yellow";
-        return;
-    }
-
-    numero = parseInt(numero);
-
-    if (isNaN(numero)) {
-        resultado.textContent = "Isso não é um número válido!";
-        resultado.style.color = "red";
-        return;
-    }
 
     if (numero % 2 === 0) {
         resultado.textContent = `O número ${numero} é PAR!`;
